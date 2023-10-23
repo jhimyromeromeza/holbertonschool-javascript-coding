6a79202e5407a5f1628fc9ae1cc808bc8f87d198
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-22 19:58:42 -0500

    c is fun

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
